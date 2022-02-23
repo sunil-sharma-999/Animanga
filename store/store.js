2582d66aea4356535751dc3bf5ec5bd8705da6f6
sunil-sharma-999 <84917190+sunil-sharma-999@users.noreply.github.com>
2022-02-23 11:36:52 +0530

    Delete store directory

--- store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import animangaReducer from './slices/animangaSlice';
-
-export const store = configureStore({
-  reducer: {
-    animanga: animangaReducer,
-  },
-});
