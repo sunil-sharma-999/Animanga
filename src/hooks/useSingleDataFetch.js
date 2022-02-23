84a591500cf675dbff8909cafc0d1afd408f363c
sunil-sharma-999 <84917190+sunil-sharma-999@users.noreply.github.com>
2022-02-23 10:42:59 +0530

    Add files via upload

--- src/hooks/useSingleDataFetch.js
+++ src/hooks/useSingleDataFetch.js
@@ -4,20 +4,22 @@ import { useEffect, useState } from 'react';
 const useSingleDataFetch = (type, id) => {
   const [data, setdata] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [err, setErr] = useState('Failed to Fetch Data');
+  const [err, setErr] = useState(null);
 
   useEffect(() => {
+    setErr(null);
     setLoading(true);
     axios
       .request(`https://api.jikan.moe/v4/${type}/${id}`)
       .then((res) => {
         setdata(res.data.data);
         setLoading(false);
+        setErr(null);
       })
       .catch((error) => {
         console.log(error);
         setLoading(false);
-        setErr(error.response.statusText);
+        setErr(error.response.statusText || 'Failed to fetch data');
       });
   }, [type, id]);
 
